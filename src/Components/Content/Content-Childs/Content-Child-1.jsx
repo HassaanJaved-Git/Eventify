a69d14f290db11062458231a30b6591585836ad1
AbdulWahid419 <ranaw8537@gmail.com>
2025-05-30 22:53:45 +0500

    i just update the events page here and add some events staticaly in the section

--- src/Components/Content/Content-Childs/Content-Child-1.jsx
+++ src/Components/Content/Content-Childs/Content-Child-1.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import img from '../../../assets/calendar.png'
+import Child1Child from './Child1-Childs/Child1-child'
+
+
 
 const ContentChild1 = () => {
   return (
    <div className="col-lg-12 col-md-12 col-sm-12 col-12">
-  <div className="bg-dark text-light text-center py-5 rounded">
-    <img src={img} alt="empty events logo" className="mb-3 empty-image" height={'10px'} />
+  <div className="bg-dark text-light text-left py-5 rounded card-lists">
+    <Child1Child />
+    <Child1Child />
+    <Child1Child />
+    {/* <img src={img} alt="empty events logo" className="mb-3 empty-image" height={'10px'} />
     <h3 className="mb-2">No Upcoming Events</h3>
     <p className="mb-4">You have no upcoming events. Why not host one?</p>
-    <button className="btn btn-outline-light">+ Create Event</button>
+    <button className="btn btn-outline-light">+ Create Event</button> */}
   </div>
 </div>
   )
