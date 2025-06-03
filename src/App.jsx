e467d445a6f25fc7db9cee54cbc50d1f01507b3d
hassanajmal21 <hassan.32aug24webbpt@gmail.com>
2025-06-03 23:06:31 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -11,45 +11,42 @@ const Dashboard = lazy(()=>import('./Component/Dashboard/dashboard'));
 const CreateEventPage = lazy(()=>import('./Pages/CreateEventPage'));
 const ForgetPassword = lazy(()=>import('./Component/Forget-Password/ForgetPassword'))
 
-// import Content from './Components/Content/Content'
-// import Footer from './Components/Footer/Footer'
+import Content from './Components/Content/Content'
+import Footer from './Components/Footer/Footer'
 import MyNavbar from './Component/Navber/Navbar';
 import UserName from './Component/UserName/UserName'
 
 import { AuthProvider } from "./Context/AuthContext";
-// import Settings from '../src/Pages/Setting';
+import Settings from './Pages/Setting';
 
-import ProfilePage from './Pages/ProfilePage';
-import Settings from './Pages/Setting'
-
-import './App.css'
-import Content from './Components/Content/Content'
-import Footer from './Components/Footer/Footer'
-// import tailwindcss from 'tailwindcss'
-import Header from "./Components/Header/Header"
+// import ProfilePage from './Pages/ProfilePage';
 
 function App() {
   return (
-    <>
-        <Header />
-        <Content />
-        <Footer />
-    
-    <BrowserRouter>
-      <AuthProvider>
-        <MyNavbar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUp />} />
-           <Route path="/create-event" element={<CreateEventPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                     <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-    </>
+    <Suspense fallback={<div className='loadDiv'>Loading...</div>} >
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <AuthProvider>
+          <MyNavbar />
+          <Routes>
+            <Route path="/*" element={"404"}/>
+            <Route path="/" element={<layout>
+              <Content />
+              <Footer />
+            </layout>} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path='/username' element={<UserName />}/>
+            <Route path="/create-event" element={<CreateEventPage />} />
+            <Route path="/forget-password" element={<ForgetPassword />} />
+             <Route path="/settings" element={<Settings />} />
+                  {/* <Route path="/profile" element={<ProfilePage />} /> */}
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+    </Suspense>
   );
-
 }
+
 export default App;
