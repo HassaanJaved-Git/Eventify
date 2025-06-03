5596c43e23610b64be48d01c4e3885bc36341134
H M <hasanmunir406@gmail.com>
2025-06-03 23:25:02 +0500

    Update

--- src/Component/Forget-Password/ForgetPassword.jsx
+++ src/Component/Forget-Password/ForgetPassword.jsx
@@ -22,7 +22,7 @@ const ForgetPassword = () => {
   const resetPasswordSchema = Yup.object({
     otp: Yup.string().required('OTP is required'),
     email: Yup.string().email('Invalid email').required('Email is required'),
-    password: Yup.string().min(8, 'Password must be at least 8 characters').max(32, 'Password must be at most 32 characters').matches(/[A-Z]/, 'Password must contain at least one uppercase letter').matches(/[a-z]/, 'Password must contain at least one lowercase letter').matches(/[0-9]/, 'Password must contain at least one number').matches(/[@$!%*?&]/, 'Password must contain at least one special character').required('Password is required'),
+    newPassword: Yup.string().min(8, 'Password must be at least 8 characters').max(32, 'Password must be at most 32 characters').matches(/[A-Z]/, 'Password must contain at least one uppercase letter').matches(/[a-z]/, 'Password must contain at least one lowercase letter').matches(/[0-9]/, 'Password must contain at least one number').matches(/[@$!%*?&]/, 'Password must contain at least one special character').required('Password is required'),
   });
 
   const handleSendOtp = (values, { setSubmitting }) => {
