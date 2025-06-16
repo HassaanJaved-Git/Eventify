372b2acd381e9419b064376d3aebf09f5495f2bc
HassaanJaved-Git <93113@gcslahore.edu.pk>
2025-06-16 21:45:00 +0500

    AbdulWahid code for nested settings options

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -8,7 +8,7 @@ const Content = () => {
   return (
     <>
       <div className='container-fluid Content-main'>
-        <div className='col-12 col-lg-10 col-md-10 col-sm-12 m-auto text-light Parent-div p-lg-5 p-md-3 p-sm-2'>
+        <div className='main-container col-12 col-lg-10 col-md-10 col-sm-12 m-auto text-light Parent-div p-lg-5 p-md-3 p-sm-2'>
           <div className='col-12 d-flex justify-content-between align-items-center'>
            <h1 className='mb-0'>Events</h1>
 
