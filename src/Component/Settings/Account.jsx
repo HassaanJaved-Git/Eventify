881368f895d41db8efad22bfb76526a2abc57029
HassaanJaved-Git <93113@gcslahore.edu.pk>
2025-06-18 18:04:58 +0500

    ..

--- src/Component/Settings/Account.jsx
+++ src/Component/Settings/Account.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'; // Added useState import
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { toast } from 'react-toastify';
 import './Account.css';
 
