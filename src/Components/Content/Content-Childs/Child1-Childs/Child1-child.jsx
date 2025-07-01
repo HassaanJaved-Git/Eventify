eb54dd0285017407efb3d1290b66c7c45e6e7d4c
H M <hasanmunir406@gmail.com>
2025-07-01 23:28:25 +0500

    Updated

--- src/Components/Content/Content-Childs/Child1-Childs/Child1-child.jsx
+++ src/Components/Content/Content-Childs/Child1-Childs/Child1-child.jsx
@@ -35,9 +35,12 @@ const Child1Child = ({ id, title, description,price, image, date, startTime, end
           { headers }
         );
 
-        setTicketId(response.data.ticketId);
+        // setTicketId(response.data.ticketId);
+        // alert("Free ticket booked!");
+        // navigate(`/ticket/${ticketId}`);
+        const ticketIdFromResponse = response.data.ticketId;
         alert("Free ticket booked!");
-        navigate(`/ticket/${ticketId}`);
+        navigate(`/ticket/${ticketIdFromResponse}`);
       } else {
         const payfastResponse = await axios.post(
           `http://localhost:5000/api/payment/initiate-payment`,
